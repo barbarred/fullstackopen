761042d34844c6159b42d3419344a94b4a49b636
rorridev <ricardo.rjose@gmail.com>
2024-05-03 16:43:15 -0400

    exercise 2.9 step 4 phonebook done!

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {}
+    {name: 'Alex', number: 1612902},
+    {name: 'Pepe', number: 64188837},
+    {name: 'Rorri', number: 1523654},
+    {name: 'Mari', number: 1478524},
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newfilter, setFilter] = useState('')
 
 
   const handleChange = (event) => {
@@ -14,6 +18,11 @@ const App = () => {
   const handleChangeNumber = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+  
+  const filterResult = persons.filter(person => person.name.toLowerCase().includes(newfilter.toLowerCase()))
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -40,7 +49,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input value={newfilter} onChange={handleFilter} />
+      </div>
       <form onSubmit={addNumber}>
+
+      <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleChange}/>
         </div>
@@ -55,7 +70,7 @@ const App = () => {
       <div>
         <ul>
           {
-            persons.map(person => 
+            filterResult.map(person => 
             <p key={Math.random()}>{person.name} {person.number}</p>
             )
           }
