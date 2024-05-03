64a2b30128a7856f66c92523914053816e7f7f6a
rorridev <ricardo.rjose@gmail.com>
2024-05-02 20:08:48 -0400

    exercise 2.8 phonebook done!

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,54 +2,65 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    {}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
 
   const handleChange = (event) => {
     setNewName(event.target.value)
   }
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
 
-  const addName = (event) => {
+  const addNumber = (event) => {
     event.preventDefault()
-    if(newName === '') return
+    if(newName === '' || newNumber === '') return
     function nameCheck(persons, check) {
       return persons.find(obj => obj.name === check) !== undefined
     }
-
-    if(nameCheck(persons, newName)){
-      alert(`${newName} is already added to phonebook`)
+    function numberCheck(persons, numCheck){
+        return persons.find(objN => objN.number === numCheck) !== undefined
+      }
+    if(nameCheck(persons, newName) || numberCheck(persons, newNumber)){
+      alert(`The name: ${newName} or the number: ${newNumber} is already added to phonebook`)
     }else{
       const newObject = {
       name: newName,
+      number: newNumber,
       }
       setPersons(persons.concat(newObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addNumber}>
         <div>
           name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleChangeNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      <h2>Numbers</h2>
       <div>
         <ul>
           {
             persons.map(person => 
-            <p key={person.name}>{person.name}</p>
+            <p key={Math.random()}>{person.name} {person.number}</p>
             )
           }
         </ul>        
       </div>
-      <h2>Numbers</h2>
-      ...
     </div>
   )
 }
