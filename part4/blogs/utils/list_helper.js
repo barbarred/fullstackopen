b7dc44a31e5b31f1e1e1903c6b15296b5dfc8a6d
rorridev <ricardo.rjose@gmail.com>
2024-06-07 19:07:47 -0400

    exercise 4.5 done!

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,11 +1,40 @@
-const dummy = (blog) => {
-  if(blog.length > 1){
+const logger = require('../utils/logger.js')
+
+const dummy = (blogs) => {
+  if(blogs.length > 1){
     return 1
   }else{
     return 1
   }
 }
 
+const totalLikes = (blogs) => {
+
+  if(blogs.length === 1){
+    let sumOfLikes = blogs[0].likes
+    return sumOfLikes
+  }else{
+    let sumOfLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    return sumOfLikes
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return []
+
+  let moreLikes = blogs[0]
+
+  for(let i = 1; i < blogs.length; i++){
+    if(blogs[i].likes > moreLikes.likes){
+      moreLikes = blogs[i]
+    }
+  }
+  return(moreLikes.likes)
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
