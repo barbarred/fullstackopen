3b9e8bad8cc4552b9b43f7a18d82ac1e236f5f7c
rorridev <ricardo.rjose@gmail.com>
2024-06-19 15:42:25 -0400

    exersice 4.10 done!

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -1,4 +1,5 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog.js')
 const assert = require('assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -6,17 +7,36 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const initialEntries = [
+  {
+    title: "first entrie for test",
+    author: "rorri",
+    url: "https://github.com/barbarred",
+    likes: 33
+  },
+  {
+    title: "another entrie for testing",
+    author: "rorridev",
+    url: "https://github.com/barbarred",
+    likes: 23
+  }
+]
+
+beforeEach( async () => {
+  await Blog.deleteMany({})
+  let entrieObject = new Blog(initialEntries[0])
+  await entrieObject.save()
+  entrieObject = new Blog(initialEntries[1])
+  await entrieObject.save()
+})
+
 test('correct json entries format', async () => {
-  await api
+  const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-})
-
-test('entries blogs in json format', async () => {
-  const response = await api.get('/api/blogs')
-
-  assert.strictEqual(response.body.length, 2)
+  
+  assert.strictEqual(response.body.length, initialEntries.length)
 })
 
 test('correct id name', async () => {
@@ -26,6 +46,27 @@ test('correct id name', async () => {
   const checkIdName = blogs[0].id ? 'id' : false
   
   assert.strictEqual(checkIdName, 'id')
+})
+
+test('one entrie created succesfully', async () => {
+
+  const newEntrie = {
+    title: "new entrie created to POST method",
+    author: "barbarred",
+    url: "https://github.com/barbarred",
+    likes: 22
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newEntrie)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialEntries.length + 1)
+
 })
 
 after(async () => {
