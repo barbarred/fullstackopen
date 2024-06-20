1e9a417e40c0a2e00edbb99b6920bc39ee9aaabd
rorridev <ricardo.rjose@gmail.com>
2024-06-20 17:42:16 -0400

    exercise 4.14 done!

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -6,14 +6,45 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body =  request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog =  new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+  if(blog.likes){
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }else if(blog.likes && blog.url){
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }else{
+    response.status(400).json('somes keys are missing')
+  }
 })
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const entrie = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedEntrie = await Blog.findByIdAndUpdate(request.params.id, entrie, {new: true})
+  response.status(200).json(updatedEntrie)
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+
 
 module.exports = blogsRouter
--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const Blog = require('../models/blog.js')
 const assert = require('assert')
 const mongoose = require('mongoose')
@@ -30,7 +30,8 @@ beforeEach( async () => {
   await entrieObject.save()
 })
 
-test('correct json entries format', async () => {
+describe('Gets correct info', () => {
+  test('correct json entries format', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
@@ -39,35 +40,93 @@ test('correct json entries format', async () => {
   assert.strictEqual(response.body.length, initialEntries.length)
 })
 
-test('correct id name', async () => {
+  test('correct id name', async () => {
 
-  const response = await api.get('/api/blogs')
-  const blogs = response.body
-  const checkIdName = blogs[0].id ? 'id' : false
-  
-  assert.strictEqual(checkIdName, 'id')
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    const checkIdName = blogs[0].id ? 'id' : false
+    
+    assert.strictEqual(checkIdName, 'id')
+  })
 })
 
-test('one entrie created succesfully', async () => {
+describe('Add entries in the blogs object', () => {
+    test('one entrie created succesfully', async () => {
 
-  const newEntrie = {
-    title: "new entrie created to POST method",
-    author: "barbarred",
-    url: "https://github.com/barbarred",
-    likes: 22
-  }
+    const newEntrie = {
+      title: "new entrie created to POST method",
+      author: "barbarred",
+      url: "https://github.com/barbarred",
+      likes: 22
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newEntrie)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newEntrie)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/blogs')
+    const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, initialEntries.length + 1)
+    assert.strictEqual(response.body.length, initialEntries.length + 1)
+
+  })
+})
+
+describe('Checking somes keys on Blogs', () => {
+    test('check likes key in object blogs', async () => {
+      const newEntrie = {
+        title: "a one entrie without the likes key",
+        author: "rorri",
+        url: "https://github.com/barbarred"
+      }
+      
+      await api
+        .post('/api/blogs')
+        .send(newEntrie)
+        .expect(400)
+    })
+
+    test('check title & url keys in the blogs object', async () => {
+      const newEntrie = {
+        title: "a one entrie without the likes key",
+        author: "rorri"
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newEntrie)
+        .expect(400)
+    })
+})
+
+describe('Delete and Update one entrie of the object blogs', () => {
+  test('succeeds deletion with the status code 204', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    const entrieToDelete = blogs[0]
+
+    await api
+      .delete(`/api/blogs/${entrieToDelete.id}`)
+      .expect(204)
+  })
+  test('The update is successful with status code 200', async () => {
+    const updateEntrie = {
+      title: "update a entrie",
+      author: "barbarred",
+      url: "https://github.com/barbarred",
+      likes: 33
+    }
+
+    await api
+      .put('/api/blogs/6674920364cf31d692ea2313')
+      .send(updateEntrie)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
 })
+
 
 after(async () => {
   await mongoose.connection.close()
