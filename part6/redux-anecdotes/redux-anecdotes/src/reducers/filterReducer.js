6a6575cca814dd3b73a1aa68bbd5130e417e9015
Ricardo <ricardo.rjose@gmail.com>
2024-11-26 13:19:45 -0300

    exercise 6.10 done

--- part6/redux-anecdotes/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,4 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
 
+const initialState = ''
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterChange(state, action){
+           return action.payload
+        }
+    }
+})
+
+/*
 const filterReducer = (state = '', action) => {
     switch (action.type) {
         case 'FILTER':
@@ -14,5 +28,7 @@ export const filterChange = (filter) => {
         payload: filter
     }
 }
+*/
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
