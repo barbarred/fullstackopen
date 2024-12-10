026bd05013f43564b8722fc8e76b4b1733fbf763
Ricardo <ricardo.rjose@gmail.com>
2024-12-10 20:47:56 -0300

    exercise 6.22 done!

--- part6/reactquery-anecdotes/query-anecdotes/src/App.jsx
+++ part6/reactquery-anecdotes/query-anecdotes/src/App.jsx
@@ -1,12 +1,21 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from '../requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateVote } from '../requests'
 
-const App = () => {
+const App = () => { 
+  const queryClient = useQueryClient()
+
+  const newVoteMotation = useMutation({
+    mutationFn: updateVote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['anecdotes']})
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    const newVote = anecdote.votes + 1
+    newVoteMotation.mutate({...anecdote, votes: newVote })
   }
 
   const result = useQuery({
